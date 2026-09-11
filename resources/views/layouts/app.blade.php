82fcd805fa3734c22f3c90e9020db581924ddebf
Apple <apple@192.168.1.3>
2026-09-11 12:52:04 +0500

    Merge remote repository

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-title" content="Faheem Chat">
     <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
     <title>@yield('title', 'Faheem Innovations')</title>
     <script src="https://cdn.tailwindcss.com"></script>
